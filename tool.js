a94980c0307361a2bddfb86ac6b7b8d8c280ee72
missedliunian <40048483+missedliunian@users.noreply.github.com>
2018-11-28 09:31:17 +0800

    Add files via upload

---
+++ tool.js
@@ -0,0 +1,29 @@
+downloadImg = (url, idx) => {
+  let image = new Image();
+  image.setAttribute("crossOrigin", "anonymous");
+  image.src = url;
+  image.onload = function() {
+    let canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    let context = canvas.getContext("2d");
+    context.drawImage(image, 0, 0, image.width, image.height);
+    let url = canvas.toDataURL("image/jpg"); //得到图片的base64编码数据
+    let a = document.createElement("a"); // 生成一个a元素
+    let event = new MouseEvent("click"); // 创建一个单击事件
+    a.download = `IMG${idx}`; // 设置图片名称
+    a.href = url; // 将生成的URL设置为a.href属性
+    a.dispatchEvent(event); // 触发a的单击事件
+  };
+};
+randomString = (len = 6) => {
+  len = len || 32;
+  let $chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+  /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+  let maxPos = $chars.length;
+  let pwd = "";
+  for (let i = 0; i < len; i++) {
+    pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return pwd;
+};
